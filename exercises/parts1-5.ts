c685ccfd255f86e1498f927d8d4e3d1ccd4094da
Tavier <84361318+Tavier@users.noreply.github.com>
2021-08-05 18:13:05 -0500

    Exercise

--- exercises/parts1-5.ts
+++ exercises/parts1-5.ts
@@ -3,34 +3,54 @@
 
 
 // Part 1: Declare (5) Variables With Type
-
-
+//let spacecraftName: string = 'Determination';
+//let speedMph: number = 17500;
+let kilometersToMars: number = 225000000;
+let kilometersToTheMoon: number = 384400;
+//let milesPerKilometer: number = 0.621;
 
 // Part 2: Print Days to Mars
+let milesToMars: number = kilometersToMars * milesPerKilometer;
+let hoursToMars: number = milesToMars / speedMph;
+let daysToMars: number = hoursToMars / 24;
 
-
-
-// Code an output statement here (use a template literal):
-
-
-
-// Part 3: Create a Function ("getDaysToLocation")
 
 
 
 // Move your output statement from part 2 here. Update the template literal to call
 // the function and print the outputs for a Mars trip and a moon trip.
+// Code an output statement here (use a template literal):
+console.log(`It will take the Spacecraft: ${spacecraftName}, ${getDaysToLocation(kilometersToMars)} hours to get to get to Mars.`);
+console.log(`It will take the Spacecraft: ${spacecraftName}, ${getDaysToLocation(kilometersToTheMoon)} hours to get to get to the Moon.`);
+
 
 
 
 
 // Part 4: Create a Spacecraft Class
-
+class Spacecraft {
+    milesPerKilometer: number = 0.621;
+    name: string;
+    speedMph: number;
+    constructor(name: string, speedMph: number, ) {
+        this.name = name;
+        this.speedMph = speedMph;
+    }
+    // Part 3: Create a Function ("getDaysToLocation")
+    getDaysToLocation(kilometersAway: number): number{
+        let milesAway: number = kilometersAway * this.milesPerKilometer;
+        let hoursToLocation: number = milesAway / this.speedMph;
+        let daysAway: number = hoursToLocation / 24;
+        return daysAway; 
+    }
+}
 
 
 
 // Create an instance of the class here:
-
+interface Spacecraft {
+    let spaceShuttle = new Spacecraft('Determination', 17500);
+}
 
 
 // Move your output statements from part 3 here. Update the template literals use the
