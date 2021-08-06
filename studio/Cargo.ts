8de1ecf766f3953728ae4c2a670e581ee34234fe
Tavier <84361318+Tavier@users.noreply.github.com>
2021-08-05 20:34:42 -0500

    adding Cargo.ts

---
+++ studio/Cargo.ts
@@ -0,0 +1,12 @@
+//Cargo.ts
+
+import { Payload } from './Payload';
+
+export class Cargo implements Payload {
+    massKg: number;
+    material: string;
+    constructor(massKg: number, material: string){
+        this.massKg = massKg;
+        this.material = material;
+    }
+}
